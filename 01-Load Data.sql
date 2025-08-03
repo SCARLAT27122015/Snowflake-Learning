0baad6e275ecb4c9b1f2e631353bb1a38f936311
Israel Gonzalez <mansion.lagrimas@gmail.com>
2025-08-02 23:11:11 -0600

    Create 01-Load Data.sql

---
+++ 01-Load Data.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE EXERCISE_DB;
+
+CREATE OR REPLACE TABLE EXERCISE_DB.PUBLIC.CUSTOMERS
+(
+    ID INT,
+    first_name varchar,
+    last_name varchar,
+    email varchar,
+    age int,
+    city varchar
+);
+
+
+COPY INTO EXERCISE_DB.PUBLIC.CUSTOMERS
+FROM s3://snowflake-assignments-mc/gettingstarted/customers.csv
+file_format = (type = csv 
+               field_delimiter = ',' 
+               skip_header=1);
+
+
+SELECT * FROM EXERCISE_DB.PUBLIC.CUSTOMERS;
